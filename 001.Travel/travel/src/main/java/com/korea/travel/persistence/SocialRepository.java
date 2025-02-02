1413efa4b0cb2de0482d82a45fc03909cd04b9f5
Ka-YJS <ljs971021@naver.com>
2025-02-02 21:54:51 +0900

    SocialRepository commit

---
+++ 001.Travel/travel/src/main/java/com/korea/travel/persistence/SocialRepository.java
@@ -0,0 +1,16 @@
+package com.korea.travel.persistence;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.korea.travel.model.SocialEntity;
+import com.korea.travel.model.SocialEntity.AuthProvider;
+
+@Repository
+public interface SocialRepository extends JpaRepository<SocialEntity, Long> {
+    Optional<SocialEntity> findBySocialId(String socialId); // socialId로 사용자 찾기
+    Optional<SocialEntity> findByEmailAndAuthProvider(String email, AuthProvider authProvider);
+    // 이메일과 인증 제공자로 사용자 찾기
+    boolean existsByEmail(String email); // 이메일 존재 여부 확인 메서드
+    boolean existsBySocialId(String socialId); // 소셜ID 존재 여부 확인 메서드
+}
