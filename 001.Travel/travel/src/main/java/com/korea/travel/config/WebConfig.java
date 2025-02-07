ad161333c9510d521b289331b721837da193f584
Ka-YJS <ljs971021@naver.com>
2025-02-07 23:01:17 +0900

    WebConfig modify_003

--- 001.Travel/travel/src/main/java/com/korea/travel/config/WebConfig.java
+++ 001.Travel/travel/src/main/java/com/korea/travel/config/WebConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-	
+	/*
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")	//모든 경로에 대해 CORS 설정
@@ -16,7 +16,7 @@ public class WebConfig implements WebMvcConfigurer {
 				.allowedHeaders("*")		//모든 헤더 허용
 				.allowCredentials(true)	//쿠키나 인증 정보를 포함한 요청 허용
 		        .exposedHeaders("Authorization");// OAuth2 리디렉션을 위한 설정 추가
-	}
+	}*/
 	
 	//업로드된 파일을 제공할 수 있도록 설정
 	@Override
