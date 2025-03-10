61509936a76aea9e948794b29eab21c19c9d0c4b
Ka-YJS <ljs971021@naver.com>
2025-03-11 03:05:24 +0900

    ListContext modify_001

--- 001.Travel/travel-react-app/src/context/ListContext.js
+++ 001.Travel/travel-react-app/src/context/ListContext.js
@@ -1,3 +1,15 @@
-import { createContext } from "react";
+import React, { createContext, useState } from "react";
 
-export const ListContext = createContext(null);
+// 초기값을 빈 배열로 설정
+export const ListContext = createContext({ list: [], setList: () => {} });
+
+// ListProvider 컴포넌트 추가
+export const ListProvider = ({ children }) => {
+  const [list, setList] = useState([]);
+
+  return (
+    <ListContext.Provider value={{ list, setList }}>
+      {children}
+    </ListContext.Provider>
+  );
+};
