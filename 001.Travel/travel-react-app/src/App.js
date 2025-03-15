53d9aa44e306c048b7946a213ce11d16042ecb80
Ka-YJS <ljs971021@naver.com>
2025-03-16 01:04:48 +0900

    App modify_001

--- 001.Travel/travel-react-app/src/App.js
+++ 001.Travel/travel-react-app/src/App.js
@@ -7,32 +7,31 @@ import Signup from "./pages/SignUp";
 import PostDetail from "./pages/PostDetail";
 import Post from "./pages/Post";
 import Map from "./pages/Map";
-import MyPage from "./pages/MyPage"; // Mypage 추가
+import MyPage from "./pages/MyPage"; 
 import { PostContext } from "./context/PostContext";
 import { UserContext } from "./context/UserContext";
-import {PlaceContext} from "./context/PlaceContext";
+import { PlaceContext } from "./context/PlaceContext";
 import MapEdit from "./pages/MapEdit";
 import { ListContext } from "./context/ListContext";
 import { CopyListContext } from "./context/CopyListContext";
 import Logo from "./pages/Logo"
 import { CopyPlaceListContext } from "./context/CopyPlaceListContext";
 import MyPost from "./pages/MyPost";
+import { ListProvider } from "./context/ListContext"; // ListProvider 임포트 확인
 
-
-  
 function App() {
   const [placeList, setPlaceList] = useState([]);
-  const [list,setList] = useState([]);
-  const [copyList,setCopyList] = useState([]);
-  const [copyPlaceList,setCopyPlaceList] = useState([]);
+  const [copyList, setCopyList] = useState([]);
+  const [copyPlaceList, setCopyPlaceList] = useState([]);
   const [postList, setPostList] = useState([]);
-  //user정보 저장useState
+  
+  // user 정보 저장 useState
   const [user, setUser] = useState(() => {
     // 새로고침 시 로컬 스토리지에서 사용자 정보 복원
     const savedUser = localStorage.getItem("user");
     return savedUser && savedUser !== "undefined" ? JSON.parse(savedUser) : null;
   });
-  const [googleUser,setGoogleUser] = useState({});
+  const [googleUser, setGoogleUser] = useState({});
   
   // user 상태가 변경될 때 로컬 스토리지에 저장
   useEffect(() => {
@@ -47,17 +46,15 @@ function App() {
     }
   }, []);
 
-
   return (
-    <UserContext.Provider value={{user,setUser,googleUser,setGoogleUser }}>
-      <PostContext.Provider value={{ postList, setPostList }}>
-        <PlaceContext.Provider value={{placeList, setPlaceList}}>
-          <ListContext.Provider value={{list, setList}}>
-              <CopyListContext.Provider value={{copyList,setCopyList}}>
-              <CopyPlaceListContext.Provider value={{copyPlaceList,setCopyPlaceList}}>
+    <ListProvider>
+      <UserContext.Provider value={{user, setUser, googleUser, setGoogleUser}}>
+        <PostContext.Provider value={{postList, setPostList}}>
+          <PlaceContext.Provider value={{placeList, setPlaceList}}>
+            <CopyListContext.Provider value={{copyList, setCopyList}}>
+              <CopyPlaceListContext.Provider value={{copyPlaceList, setCopyPlaceList}}>
                 <div className="AppWrapper">
                   <Router>
-                    {/* <Logo /> */}
                     <Routes>
                       <Route path="/" element={<HomeScreen />} />
                       <Route path="/main" element={<MainScreen />} />
@@ -67,16 +64,16 @@ function App() {
                       <Route path="post" element={<Post />} />
                       <Route path="postEdit/:id" element={<MapEdit />} />
                       <Route path="map" element={<Map />} />
-                      <Route path="/mypost/:id" element={<MyPost />} /> {/* Mypage 경로 추가 */}
+                      <Route path="/mypost/:id" element={<MyPost />} />
                     </Routes>
                   </Router>
                 </div>
-                </CopyPlaceListContext.Provider>
-              </CopyListContext.Provider>
-          </ListContext.Provider>
-        </PlaceContext.Provider>      
-      </PostContext.Provider>
-    </UserContext.Provider>
+              </CopyPlaceListContext.Provider>
+            </CopyListContext.Provider>
+          </PlaceContext.Provider>
+        </PostContext.Provider>
+      </UserContext.Provider>
+    </ListProvider>
   );
 }
 
